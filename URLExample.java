6e621a82fae5a1e82861986fd64218161c87888c
Deepa Bhattarai <deepabhattarai463@gmail.com>
2025-09-20 18:15:30 +0545

    Added URLExample.java

---
+++ URLExample.java
@@ -0,0 +1,59 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@SuppressWarnings("deprecation")
+public class URLExample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://user:pass@www.google.com:8080/search?q=java+networking");
+
+            String protocol = url.getProtocol();
+            System.out.println("Protocol: " + protocol);
+
+            String host = url.getHost();
+            System.out.println("Host: " + host);
+
+            int port = url.getPort();
+            System.out.println("Port: " + port);
+
+            String file = url.getFile();
+            System.out.println("File: " + file);
+
+            String authority = url.getAuthority();
+            System.out.println("Authority: " + authority);
+
+            String userInfo = url.getUserInfo();
+            System.out.println("User Info: " + userInfo);
+
+            String stringUrl = url.toString();
+            System.out.println("URL: " + stringUrl);
+
+        } catch (MalformedURLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
+
+
+// 
+
+            // String protocol = url.getProtocol();
+            // System.out.println("Protocol: " + protocol);
+
+            // String host = url.getHost();
+            // System.out.println("Host: " + host);
+
+            // int port = url.getPort();
+            // System.out.println("Port: " + port);
+
+            // String file = url.getFile();
+            // System.out.println("File: " + file);
+
+            // String authority = url.getAuthority();
+            // System.out.println("Authority: " + authority);
+
+            // String userInfo = url.getUserInfo();
+            // System.out.println("User Info: " + userInfo);
+
+            // String stringUrl = url.toString();
+            // System.out.println("URL: " + stringUrl);
